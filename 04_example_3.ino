5a9d5e14c8199308092fcfb464c3e57b589d10eb
hanjungyo <66051632+hanjungyo@users.noreply.github.com>
2020-09-14 19:32:02 +0900

    Update 04_example_3.ino

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -14,7 +14,7 @@ void setup() {
 }
 
 void loop() {
-  
+  Serial.println(++count);
   toggle = toggle_state(toggle); //toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); // wait for 1,000 milliseconds
